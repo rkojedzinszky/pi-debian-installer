f1deed8755e92af8884dacfe7381db7b27843b80
Richard Kojedzinszky <krichy@nmdps.net>
2018-05-26 19:49:54 +0200

    Fix bpi-m2-plus install
    - Install initramfs-tools unconditionally
    - Removed legacy boot.cmd.template for m2-plus

--- boards/bpi-m2-plus/install.sh
+++ boards/bpi-m2-plus/install.sh
@@ -4,7 +4,7 @@ post_debootstrap()
 {
 	chroot $rootdir apt-get install -y --no-install-recommends gnupg2 dirmngr
 	chroot $rootdir apt-key adv --keyserver keys.gnupg.net --recv-keys 0x93D6889F9F0E78D5
-	echo "deb http://apt.armbian.com jessie main utils" > $rootdir/etc/apt/sources.list.d/armbian.list
+	echo "deb http://apt.armbian.com stretch main utils" > $rootdir/etc/apt/sources.list.d/armbian.list
 	chroot $rootdir apt-get update
-	chroot $rootdir apt-get -f -y --no-install-suggests install linux-image-next-sunxi initramfs-tools
+	chroot $rootdir apt-get -f -y install linux-image-next-sunxi
 }
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 TARGET_ARCH=armhf
 TARGET_DIST=stretch
 DEB_MIRROR=http://dev.euronetrt.hu:3142/debian/
-PACKAGES=firmware-brcm80211,e2fsprogs,vim,u-boot-tools,cpufrequtils
+PACKAGES=firmware-brcm80211,e2fsprogs,vim,u-boot-tools,cpufrequtils,initramfs-tools
 USE_LVM=yes
 ROOT_SIZE=2048M
 SWAP_SIZE=1024M
