b2a5c07036a054eb3ee31fac88aa10d57d0ca6b8
Richard Kojedzinszky <kojedzinszky.richard@euronetrt.hu>
2018-06-18 10:15:42 +0200

    board: orangepi-plus2e

---
+++ boards/orangepi-plus2e/install.sh
@@ -0,0 +1,12 @@
+# shell fragment
+
+DTB=/usr/lib/linux-image-next-sunxi/sun8i-h3-orangepi-plus2e.dtb
+
+post_debootstrap()
+{
+	chroot $rootdir apt-get install -y --no-install-recommends gnupg2 dirmngr
+	chroot $rootdir apt-key adv --keyserver keys.gnupg.net --recv-keys 0x93D6889F9F0E78D5
+	echo "deb http://apt.armbian.com stretch main utils" > $rootdir/etc/apt/sources.list.d/armbian.list
+	chroot $rootdir apt-get update
+	chroot $rootdir apt-get -f -y install linux-image-next-sunxi
+}
