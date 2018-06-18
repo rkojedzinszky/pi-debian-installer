3747c98ae58d3e04f300691928a10693435530f3
Richard Kojedzinszky <kojedzinszky.richard@euronetrt.hu>
2018-06-18 10:17:12 +0200

    bpi-m2-plus: installing dtb from kernel

--- boards/bpi-m2-plus/install.sh
+++ boards/bpi-m2-plus/install.sh
@@ -1,5 +1,7 @@
 # shell fragment
 
+DTB=/usr/lib/linux-image-next-sunxi/sun8i-h3-bananapi-m2-plus.dtb
+
 post_debootstrap()
 {
 	chroot $rootdir apt-get install -y --no-install-recommends gnupg2 dirmngr
