c15c99d7ab759812cbeb43f6c66fd7069b7fa73e
Richard Kojedzinszky <richard@kojedz.in>
2019-11-19 20:05:28 +0100

    installing -ks kernel

--- boards/common/install.sh
+++ boards/common/install.sh
@@ -31,7 +31,7 @@ post_debootstrap()
 
 install_kernel()
 {
-	# install krichy kernel
+	# install krichy server kernel
 	chroot $rootdir apt-get install -y --no-install-recommends gnupg2 dirmngr
 	chroot $rootdir apt-key adv --keyserver keys.gnupg.net --recv-keys 0x2ADADF37C3C302A7BADABCC10D946CE6DD9F32EB
 	echo "deb http://apt.srv.kojedz.in/ ${TARGET_DIST} main" > $rootdir/etc/apt/sources.list.d/apt.srv.kojedz.in.list
