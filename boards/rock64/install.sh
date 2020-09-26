2bd31e847405ef15e1637c0c4b994a3df19ba21c
Richard Kojedzinszky <richard@kojedz.in>
2020-09-26 09:08:45 +0200

    rock64: idbloader from rkbin

--- boards/rock64/install.sh
+++ boards/rock64/install.sh
@@ -8,5 +8,6 @@ DISKLABEL_FIRST_LBA=32768
 post_partitioning()
 {
 	dd if=idbloader.img of=$dev seek=64
-	dd if=u-boot.itb of=$dev seek=16384
+	dd if=uboot.img of=$dev seek=16384
+	dd if=trust.img of=$dev seek=24576
 }
