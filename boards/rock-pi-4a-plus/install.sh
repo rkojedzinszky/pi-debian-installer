980b7d78a27dc2d825aa9d55dca9ed822745180b
Richard Kojedzinszky <richard@kojedz.in>
2022-03-12 19:15:03 +0100

    Add board rock-pi-4a-plus

---
+++ boards/rock-pi-4a-plus/install.sh
@@ -0,0 +1,14 @@
+# shell fragment
+
+TARGET_ARCH=arm64
+KERNEL=linux-image-arm64
+
+DISKLABEL_TYPE=gpt
+DISKLABEL_FIRST_LBA=32768
+
+post_partitioning()
+{
+	dd if=idbloader.img of=$dev seek=64
+	dd if=uboot.img of=$dev seek=16384
+	dd if=trust.img of=$dev seek=24576
+}
