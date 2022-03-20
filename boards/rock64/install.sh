f82cee89b10d5bd93411eadbb45c5c755b1c19b0
Richard Kojedzinszky <richard@kojedz.in>
2022-03-20 20:07:01 +0100

    rock64: update u-boot

--- boards/rock64/install.sh
+++ boards/rock64/install.sh
@@ -8,6 +8,5 @@ DISKLABEL_FIRST_LBA=32768
 post_partitioning()
 {
 	dd if=idbloader.img of=$dev seek=64
-	dd if=uboot.img of=$dev seek=16384
-	dd if=trust.img of=$dev seek=24576
+	dd if=u-boot.itb of=$dev seek=16384
 }
