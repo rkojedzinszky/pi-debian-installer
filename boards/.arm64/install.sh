c751d33780cd58b9514bcb9b8c0f2832f2d72915
Richard Kojedzinszky <richard@kojedz.in>
2022-04-28 21:13:02 +0200

    arm64: set console to ttyS2

--- boards/.arm64/install.sh
+++ boards/.arm64/install.sh
@@ -1,5 +1,5 @@
 # replace bootz with booti
 pre_mkbootscr()
 {
-	sed -i -e "s/console=ttyS0,115200/console=ttyS0,1500000/" -e "s/^bootz/booti/" "$rootdir/boot/boot.cmd"
+	sed -i -e "s/console=ttyS0,115200/console=ttyS2,1500000/" -e "s/^bootz/booti/" "$rootdir/boot/boot.cmd"
 }
